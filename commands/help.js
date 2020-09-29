ea24b8e0803cb9a6586b6d475e3913368f9f6c5c
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-09-29 10:56:02 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,6 +14,7 @@ module.exports = {
         .addField('`kick`', 'Kicks a member from your server via mention or ID')
         .addField('`ban`', 'Bans a member from your server via mention or ID')
         .addField('`clear`', 'Purges messages')
+        .setFooter("This Command will expire within 2min!")
         .setTimestamp()
 
         const fun = new Discord.MessageEmbed()
@@ -29,21 +30,28 @@ module.exports = {
         .addField('`weather`', 'Checks weather forecast for provided location')
         .setTimestamp()
 
-        const utility = new Discord.MessageEmbed()
+        const music = new Discord.MessageEmbed()
         .setTitle('Music')
-        .addField('`Play(P)`', )
-        .addField('`Search(Sc)`')
-        .addField('`Skip(Sk)`')
-        .addField('`Stop(St)`')
-        .addField('`Pause(Pa)`')
-        .addField('`Resume(R)`')
-        .addField('`NowPlaying(Np)`')
+        .setDescription(`
+1) \`play(p)\`  
+2) \`search(sc)\`
+3) \`skip(sk)\`
+4) \`stop(st)\`
+5)  \`pause(pa)\`
+6) \`resume(r)\`
+7) \`nowplaying(np)\`
+8) \`queue(q)\`
+9) \`volume(v)\`
+10) \`invite(inv)\`
+11) \`serverinfo(si)\`
+12) \`botinfo(bi)\``)
         .setTimestamp()
 
         const pages = [
                 moderation,
                 fun,
-                utility
+                utility,
+                music
         ]
 
         const emojiList = ["⏪", "⏩"];
