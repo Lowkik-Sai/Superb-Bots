c06e45b26361cebc23f3051a27b6d3993e76e01c
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-09-29 14:15:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,37 +11,10 @@ const { join } = require('path');
 const config = require('./config.json');
 client.config = config;
 
-const db = require('quick.db');
-
-const express = require('express'),
-    cors = require('cors'),
-    app = express(),
-    port = process.env.PORT || 3000;
-    bodyParser = require('body-parser'),
-    todoRoutes = require('./routes/todo'),
-    path = require("path");
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
-app.use(express.static(path.join(__dirname, "client", "build")));
-
-app.get('/', function (req, res){
-    res.send('Root route')
-});
-
-app.use('/api/todos', todoRoutes);
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-
+const port = process.env.PORT || 3000;
 app.listen(port, function(){
     console.log("Express server is running on port " + port)
-});
-
-
+}) 
 
 const { GiveawaysManager } = require('discord-giveaways');
 
