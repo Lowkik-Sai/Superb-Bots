7708d2c0b23b230d6366a0f311f453277778a519
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-09-29 18:18:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,17 +12,7 @@ const config = require('./config.json');
 client.config = config;
 
 
-# port (as described above) and host are both wrong
-const host = 'localhost';
-const port = 3000;
 
-# use alternate localhost and the port Heroku assigns to $PORT
-const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
-
-app.listen(port, host, function() {
-  console.log("Server started.......");
-});
 
 const { GiveawaysManager } = require('discord-giveaways');
 
