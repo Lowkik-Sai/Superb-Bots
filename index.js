8371deed50065181e0a5158df3588920c9442279
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-09-29 09:57:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,80 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const { token, default_prefix } = require('./.env');
+const { token, default_prefix } = require('./config.json');
 
 const { readdirSync } = require('fs');
 
 const { join } = require('path');
 
-const config = require('./.env');
-
+const config = require('./config.json');
 client.config = config;
 
 const db = require('quick.db');
 
-require('./.env').config();
+const { GiveawaysManager } = require('discord-giveaways');
 
-require("./server.js");
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./giveaways.json",
+    updateCountdownEvery: 5000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: [],
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
+
+client.commands= new Discord.Collection();
+//You can change the prefix if you like. It doesn't have to be ! or ;
+const commandFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "commands", `${file}`));
+    client.commands.set(command.name, command);
+}
+
+
+client.on("error", console.error);
+
+client.on('ready', () => {
+    console.log('I am ready');
+    client.user.setActivity(`If I'm online, you might be recorded`)
+});
+
+
+let stats = {
+    serverID: '<ID>',
+    total: "<ID>",
+    member: "<ID>",
+    bots: "<ID>"
+}
+
+
+
+client.on('guildMemberAdd', member => {
+    if(member.guild.id !== stats.serverID) return;
+    client.channels.cache.get(stats.total).setName(`Total Users: ${member.guild.memberCount}`);
+    client.channels.cache.get(stats.member).setName(`Members: ${member.guild.members.cache.filter(m => !m.user.bot).size}`);
+    client.channels.cache.get(stats.bots).setName(`Bots: ${member.guild.members.cache.filter(m => m.user.bot).size}`);
+})
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const { token, default_prefix } = require('./config.json');
+
+const { readdirSync } = require('fs');
+
+const { join } = require('path');
+
+const config = require('./config.json');
+client.config = config;
+
+const db = require('quick.db');
+
+
 
 const { GiveawaysManager } = require('discord-giveaways');
 
@@ -98,4 +157,5 @@ client.on("message", async message => {
         }
     }
 })
-client.login(process.env.token);
+
+client.login(token);
